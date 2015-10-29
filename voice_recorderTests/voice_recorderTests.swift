222b13d1bcebd5bbbea835b7b7234ac8d2f1b1c7
Babar Bashir <babarbashir@Babars-MacBook-Pro.local>
2015-10-30 00:57:42 +0300

    Initial Commit

---
+++ voice_recorderTests/voice_recorderTests.swift
@@ -0,0 +1,36 @@
+//
+//  voice_recorderTests.swift
+//  voice_recorderTests
+//
+//  Created by Babar Bashir on 10/30/15.
+//  Copyright (c) 2015 appstar. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class voice_recorderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
